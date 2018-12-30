f43e2ef138cbb6643bd40c51e0c82cb46b33dd20
Aenyatia Nirvalen <aenyatia@gmail.com>
2018-12-30 13:46:50 +0100

    Add validation to user and vehicle.

--- Passenger.Core/Domain/User.cs
+++ Passenger.Core/Domain/User.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Passenger.Core.Domain
 {
@@ -13,27 +14,60 @@ namespace Passenger.Core.Domain
 		public DateTime CreatedAt { get; private set; }
 		public DateTime UpdatedAt { get; private set; }
 
-		private User()
-		{
-		}
-
 		public User(string email, string password, string salt, string username)
 		{
 			Id = Guid.NewGuid();
-			Email = email.ToLowerInvariant();
-			Password = password;
+			SetEmail(email);
+			SetPassword(password);
+			SetUsername(username);
 			Salt = salt;
-			Username = username;
 
 			CreatedAt = DateTime.UtcNow;
 		}
 
-		private void SetEmail(string email)
+		public void SetUsername(string username)
+		{
+			if (string.IsNullOrWhiteSpace(username))
+				throw new ArgumentException("Username is required.", nameof(username));
+
+			if (Regex.IsMatch(username, @"^[\w]"))
+				throw new ArgumentException("Invalid username.", nameof(username));
+
+			if (Username == username)
+				return;
+
+			Username = username;
+			UpdatedAt = DateTime.UtcNow;
+		}
+
+		public void SetEmail(string email)
 		{
 			if (string.IsNullOrWhiteSpace(email))
 				throw new ArgumentException("Email is required.", nameof(email));
 
+			if (Regex.IsMatch(email, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", RegexOptions.IgnoreCase))
+				throw new ArgumentException("Invalid email.", nameof(email));
 
+			if (Email == email)
+				return;
+
+			Email = email.ToLowerInvariant();
+			UpdatedAt = DateTime.UtcNow;
+		}
+
+		public void SetPassword(string password)
+		{
+			if (string.IsNullOrWhiteSpace(password))
+				throw new ArgumentException("Password is required.", nameof(password));
+
+			if (Regex.IsMatch(password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,15}$"))
+				throw new ArgumentException("Invalid password.", nameof(password));
+
+			if (Password == password)
+				return;
+
+			Password = password;
+			UpdatedAt = DateTime.UtcNow;
 		}
 	}
 }
--- Passenger.Core/Domain/Vehicle.cs
+++ Passenger.Core/Domain/Vehicle.cs
@@ -1,4 +1,6 @@
-﻿namespace Passenger.Core.Domain
+﻿using System;
+
+namespace Passenger.Core.Domain
 {
 	public sealed class Vehicle
 	{
@@ -6,9 +8,38 @@
 		public string Model { get; private set; }
 		public int Seats { get; private set; }
 
-		public Vehicle()
+		public Vehicle(string brand, string model, int seats)
 		{
-			
+			SetBrand(brand);
+			SetModel(model);
+			SetSeats(seats);
+		}
+
+		private void SetBrand(string brand)
+		{
+			if (string.IsNullOrWhiteSpace(brand))
+				throw new ArgumentException("Brand is required.", nameof(brand));
+
+			Brand = brand;
+		}
+
+		private void SetModel(string model)
+		{
+			if (string.IsNullOrWhiteSpace(model))
+				throw new ArgumentException("Model is required.", nameof(model));
+
+			Model = model;
+		}
+
+		private void SetSeats(int seats)
+		{
+			if (seats < 0)
+				throw new ArgumentException("Invalid value.", nameof(seats));
+
+			if (seats > 10)
+				throw new ArgumentException("Invalid value.", nameof(seats));
+
+			Seats = seats;
 		}
 	}
 }
