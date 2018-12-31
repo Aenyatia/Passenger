fbb2256312e2a9a34e4ee796a4cf60b9fc76594c
Aenyatia Nirvalen <aenyatia@gmail.com>
2018-12-31 22:15:57 +0100

    Fluent task handler.

--- Passenger.Infrastructure/Handlers/User/LoginHandler.cs
+++ Passenger.Infrastructure/Handlers/User/LoginHandler.cs
@@ -12,21 +12,28 @@ namespace Passenger.Infrastructure.Handlers.User
 		private readonly IUserService _userService;
 		private readonly IJwtHandler _jwtHandler;
 		private readonly IMemoryCache _memoryCache;
+		private readonly IHandler _handler;
 
-		public LoginHandler(IUserService userService, IJwtHandler jwtHandler, IMemoryCache memoryCache)
+		public LoginHandler(IUserService userService, IJwtHandler jwtHandler, IMemoryCache memoryCache, IHandler handler)
 		{
 			_userService = userService;
 			_jwtHandler = jwtHandler;
 			_memoryCache = memoryCache;
+			_handler = handler;
 		}
 
 		public async Task Handle(Login command)
 		{
-			await _userService.Login(command.Email, command.Password);
-			var user = await _userService.Get(command.Email);
-			var jwt = _jwtHandler.CreateToken(user.Id, user.Role);
+			await _handler.Run(async () => await _userService.Login(command.Email, command.Password))
+				.Next()
+				.Run(async () =>
+				{
+					var user = await _userService.Get(command.Email);
+					var jwt = _jwtHandler.CreateToken(user.Id, user.Role);
 
-			_memoryCache.SetJwt(command.TokenId, jwt);
+					_memoryCache.SetJwt(command.TokenId, jwt);
+				})
+				.ExecuteAsync();
 		}
 	}
 }
